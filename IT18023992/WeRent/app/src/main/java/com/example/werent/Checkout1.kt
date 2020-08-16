d82b98b6147f03496388b00b510b7d5ac2b31ffa
nilanmeegoda99 <nilanmeegoda@gmail.com>
2020-08-17 00:38:26 +0530

    Adding all the members source codes

---
+++ IT18023992/WeRent/app/src/main/java/com/example/werent/Checkout1.kt
@@ -0,0 +1,11 @@
+package com.example.werent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Checkout1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout1)
+    }
+}
